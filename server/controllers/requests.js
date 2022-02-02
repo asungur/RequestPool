9c8163b019139a184222bcea48371bcb1b762407
ahtrotta <ahtrotta@gmail.com>
2022-02-01 21:47:58 -0500

    added more logic for handling POST requests to a bin

--- server/controllers/requests.js
+++ server/controllers/requests.js
@@ -17,13 +17,22 @@ requestRouter.post('/r/:hash', (request, response) => {
   const hash = request.params.hash;
 
   // query posgres for all entries
-  postgres.query('SELECT * FROM test WHERE hash = $1', [hash], (error, result) => {
+  postgres.query('SELECT * FROM test WHERE hash = $1', [hash], (error, results) => {
     if (error) {
       throw error;
     }
 
-    // send JSON to the frontend
-    response.status(200).json(results.rows);
+    // if hash isn't in database, return 404
+    if (results.length === 0) {
+      response.status(404);
+    }
+
+    // if hash is in database, create a record in the database
+    postgres.query('', [hash], (error, results) => {
+      if (error) {
+        throw error;
+      }
+    });
   });
 
 });
