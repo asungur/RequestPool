0d68ae432a9722d8e9b9e44144ef0d7134479b08
Reilly Knutson <Reillyknut@gmail.com>
2022-02-02 16:09:53 -0500

    Added sql setup file and hashing function to post hashed url data to postgres

--- server/controllers/binsController.js
+++ server/controllers/binsController.js
@@ -4,24 +4,43 @@ const postgres = require('../db/postgres');
 const getBin = (req, res, next) => {
   const hash = req.params.hash;
   // query posgres (move this to a model and use promises here)
-  postgres.query('SELECT * FROM test WHERE hash = $1', [hash], (error, results) => {
+  postgres.query('SELECT * FROM bins WHERE id = $1', [hash], (error, results) => {
     if (error) {
       throw error;
     }
     // send JSON to the frontend
-    response.status(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 }
 
 const createBin = (req, res, next) => {
-  const currentDate = new Date().toLocaleString();
-  // call hash function
+  // call hash function based on current time
+  const timeNow = new Date()
+  const createTime = timeNow.getTime()
+
+  // this should give a unique hash value every time for the next 100 years or so
+  // may not even need to query the DB first but we probably should
+  const hash = createTime.toString(36)
+  const updateTime = timeNow.setHours(timeNow.getHours() + 48)
+
   // check postgres for string (move this to a model and use promises here)
-  postgres.query('SELECT * FROM test WHERE hash = $1', [hash], (error, results) => {
+  postgres.query('SELECT * FROM bins WHERE id = $1', [hash], (error, results) => {
     if (error) {
       throw error;
     }
   });
+
+  // (move this to a model and use promises here)
+  // postgres timestamps are in seconds while js are in ms - divide by 1000.0 so there's no data loss
+  postgres.query(`INSERT into bins (id, created_at, update_at)
+              VALUES ('${hash}', to_timestamp(${createTime} / 1000.0),
+              to_timestamp(${updateTime} / 1000.0));`,
+  (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(201).send(`Hash successfully generated: ${hash}`)
+  })
 }
 
 exports.getBin = getBin;
